4d5de4548dc163eaf0d9d5c8d14305afeb67574f
Vishal <vishal.c@foxberry.in>
2026-08-23 23:33:08 +0530

    resolved mondodb file issue

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
 
-process.loadEnvFile();
+const envPath = path.resolve(__dirname, "../.env");
+if (!process.env.MONGODB_URI && fs.existsSync(envPath)) {
+    process.loadEnvFile(envPath);
+}
 
 const connectDB = async () => {
     try {
+        if (!process.env.MONGODB_URI) {
+            throw new Error("MONGODB_URI is not configured");
+        }
+
         await mongoose.connect(process.env.MONGODB_URI);
 
         console.log("MongoDB connected successfully");
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const connectDB = require('./db/mongodb')
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 connectDB()
     .then(function () {
         app.listen(PORT, () => {
