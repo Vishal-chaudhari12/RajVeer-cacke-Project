fd12767983c627aab39f585a30a73e4dadf4676d
Vishal <vishal.c@foxberry.in>
2026-08-24 09:53:04 +0530

    changes in app.js add cors in allow origin

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 const allowedOrigins = [
     process.env.FRONTEND_URL,
+    "https://rajveer-cake-4ti2.onrender.com",
     "http://localhost:5173",
     "http://localhost:3000",
     "http://localhost:4200",
